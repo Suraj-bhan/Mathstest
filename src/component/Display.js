d061616dac08a7f8e78539134849ff12a94d401d
Suraj-bhan <2018mmb1296@iitrpr.ac.in>
2021-04-24 23:56:50 +0530

    commit2

--- src/component/Display.js
+++ src/component/Display.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import "../App.css"
 
-function display() {
+function Display() {
+
+    const[minVal,setMinVal] = useState(0);
+    const[maxVal,setMaxVal] = useState(100);
+    const[randomNum, setRandomNum] = useState(Math.floor(Math.random()* (maxVal - minVal + 1) + minVal));
+   
+    const handleRandomNum = () => {
+        setRandomNum(Math.floor(Math.random()* (maxVal - minVal + 1) + minVal));
+    }   
+    useEffect(() => {
+        const interval = setInterval(() => {
+          handleRandomNum();
+        }, 2000);
+        return () => clearInterval(interval);
+      }, []);
+   
     return (
         <div>
             <h2>Welcome Student</h2>
             <div className="Numcontainer">
-             <h2>Number</h2>
+             <h1 className="numberdisplay">{randomNum}</h1>
             </div>
             
         </div>
     )
+  
 }
 
-export default display
+export default Display
