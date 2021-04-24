369482285cf02e0eab7df36b513b60a729bf2b58
Suraj-bhan <2018mmb1296@iitrpr.ac.in>
2021-04-24 22:45:06 +0530

    commit1

---
+++ src/component/Display.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function display() {
+    return (
+        <div>
+            <h2>Welcome Student</h2>
+            <div className="Numcontainer">
+             <h2>Number</h2>
+            </div>
+            
+        </div>
+    )
+}
+
+export default display
